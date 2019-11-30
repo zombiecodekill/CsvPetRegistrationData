895e7de68c67e2a37923d1457d0d05b7ef035a86
Kevin O'Shaughnessy <zombiecodekill@users.noreply.github.com>
2019-11-30 09:18:57 +0000

    Delete IFileWriter.cs

--- IFileWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace PetReporting
-{
-    public interface IFileWriter
-    {
-        void WriteAllLines(string filename, List<string> csvStrings);
-    }
-}
