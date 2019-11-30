045fd073ae114a15911591096c8103418433634b
Kevin O'Shaughnessy <zombiecodekill@users.noreply.github.com>
2019-11-30 09:19:18 +0000

    Delete Owner.cs

--- Owner.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PetReporting
-{
-    public class Owner
-    {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Fullname { get; set; }
-    }
-}
