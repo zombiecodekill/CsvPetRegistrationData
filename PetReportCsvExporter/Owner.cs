2f8a6ce7b771f18503b897ff89c7bb39c40f662e
Kevin O'Shaughnessy <zombiecodekill@users.noreply.github.com>
2019-12-01 10:14:54 +0000

    Delete Owner.cs

--- PetReportCsvExporter/Owner.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PetReportCsvExporter
-{
-    public class Owner
-    {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Fullname { get; set; }
-    }
-}
